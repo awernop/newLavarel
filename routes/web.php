a571f647264297f6e43f14071eb3c9ad92c322d2
awernop <govnomamonta505@gmail.com>
2024-09-26 03:29:52 +0500

    fdkehif

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+/*Route::get('/', function () {
+    return view('home');
 });
+
+Route::get('/array', function () {
+    return view('array');
+});*/
+
+Route::get('/', [MainController::class, 'showIndex']) -> name('home');
+
+Route::get('/array', [MainController::class, 'showArray']) -> name('array');
