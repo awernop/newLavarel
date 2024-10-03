0eb7bd4224dcde4264f052627d90ba06c1485312
awernop <govnomamonta505@gmail.com>
2024-10-03 20:17:43 +0500

    edit migrations

--- database/migrations/2024_10_03_151006_create_reports_table.php
+++ database/migrations/2024_10_03_151006_create_reports_table.php
@@ -11,9 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('statuses', function (Blueprint $table) {
+        Schema::create('report', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('number');
+            $table->text('description');
             $table->timestamps();
         });
     }
@@ -23,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('statuses');
+        Schema::dropIfExists('report');
     }
 };
