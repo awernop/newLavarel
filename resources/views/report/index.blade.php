f57ba8895c6b4c5cdc021a8e347abd0bee07c2ac
awernop <govnomamonta505@gmail.com>
2024-10-26 18:51:40 +0500

    create delete function

--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -5,6 +5,11 @@
       <div class='card'>
         <span class='card__number'>{{$report['number']}}</span>
         <p class='card__description'>{{$report['description']}}</p>
+        <form method="POST" action="{{route('reports.destroy', $report->id)}}">
+          @method('delete')
+          @csrf
+          <input type="submit" value="Удалить">
+        </form>
       </div>
     @endforeach
  </div>
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,5 @@ Route::get('/', [MainController::class, 'showIndex']) -> name('home');
 Route::get('/array', [MainController::class, 'showArray']) -> name('array');
 
 Route::get('/reports', [ReportController::class, 'index']) -> name('report.index');
+
+Route::delete('/reports/{report}', [ReportController::class, 'destroy']) -> name('reports.destroy');
