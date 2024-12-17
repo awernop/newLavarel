d00f1816139f0e3c981e99cd75820067d87f7214
awernop <govnomamonta505@gmail.com>
2024-12-17 23:36:45 +0500

    косметические изменения

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,9 @@ import './statusColor.js';
 import Alpine from 'alpinejs';
 import mask from '@alpinejs/mask'
 import 'flowbite';
+import.meta.glob([
+      '../images/**',
+   ]);
 
 window.Alpine = Alpine;
 Alpine.plugin(mask);
